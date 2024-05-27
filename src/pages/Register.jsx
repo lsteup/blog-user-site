d515bec7be328cef9efbf47d8567e1eb3ca848f4
lsteup <loras12@gmail.com>
2024-05-27 17:19:23 +0200

    set up toggle member functionality in register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -20,13 +20,43 @@ const Register = () => {
     console.log(e.target);
   };
 
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+    console.log(values);
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <Logo />
-        <h3>Login</h3>
-        <FormRow />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
         <button type="submit">Login</button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button type="button" onClick={toggleMember}>
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
       </form>
     </div>
   );
