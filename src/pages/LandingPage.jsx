4c59dc45a83521e503c101ed95fae68e06d6da2f
lsteup <loras12@gmail.com>
2024-05-29 20:57:51 +0200

    adjust styling for landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,23 +4,28 @@ import bird from "/bird.png";
 
 const LandingPage = () => {
   return (
-    <main className="bg-stone-200 h-screen">
-      <nav className="px-16 flex items-center border-b border-black gap-4 font-light text-sm">
-        <div className="flex items-center">
+    <main className="bg-stone-200 h-screen flex flex-col space-between">
+      <nav className="px-16 flex items-center border-b border-black gap-8 font-light text-sm">
+        <div className="flex items-center gap-2">
           <Logo />
-          <p className="text-3xl font-bold font-serif">BlogSpace</p>
+          <p className="text-2xl font-semibold font-serif">Blog</p>
         </div>
         <Link className="grow text-right">Membership</Link>
         <Link to="register">Sign in</Link>
         <Button link="register" text="Get started" />
       </nav>
-      <div className="relative px-16 font-serif flex flex-col justify-center h-full gap-8">
-        <h1 className="text-9xl ">Share your story here ...</h1>
+      <div className="relative px-16 font-serif flex flex-col justify-center grow  gap-8">
+        <h1 className="text-9xl after:content-['|'] after:animate-ping after:font-light  ">
+          Share your story here
+        </h1>
         <p className="font-sans text-2xl font-light">
           Where Reading, Writing, and Insight Converge.
         </p>
         <img className="absolute w-1/4 top-0 right-10 " src={bird} alt="" />
         <Button link="register" text="Get started" />
+      </div>
+      <div className="border-t border-black text-center py-4">
+        copyright lsteup
       </div>
     </main>
   );
